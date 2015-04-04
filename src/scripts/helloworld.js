9ef0d9e14adfe368db1d4c2af1d4383ec0d3c37d
angolinha <xsustek.martin@gmail.com>
2015-04-04 12:08:02 +0200

    Pridanie nutneho angular routovania. Pridanie examplu na controller

--- src/scripts/helloworld.js
+++ src/scripts/helloworld.js
@@ -1,1 +1,5 @@
-angular.module('helloWorld', []);
+var helloWorld = angular.module('helloWorld', ['ngRoute']);
+
+helloWorld.controller('UserController', ['$scope', '$route', function($scope, $route) {
+  $scope.username2 = "Homer Simpson";
+}]);
