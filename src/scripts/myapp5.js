ad049303987dd94319908d41d7d096b7b869a7e0
angolinha <xsustek.martin@gmail.com>
2015-04-04 20:38:57 +0200

    Pridanie prikladu na filtrovanie pajpovanim

---
+++ src/scripts/myapp5.js
@@ -0,0 +1,15 @@
+deck.on('activate', function(ev, el){
+  if(ev.index == 12){
+    console.clear();
+    angular.bootstrap(angular.element('.myapp5'), ['myapp5']);
+  }
+});
+
+angular.module('myapp5', ['ngRoute'])
+  .controller('WorkersController', ['$scope', '$route' , function($scope, $route) {
+    $scope.workers = [
+        {name: "Homer", position: "Bezpečnostný technik"},
+        {name: "Lenny", position: "Jadrový inžinier"},
+        {name: "Carl", position: "Vedúci prevádzky"}
+    ];
+  }]);
